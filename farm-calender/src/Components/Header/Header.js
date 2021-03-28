e9125eea3aa10c0be2b55d7a785ab0d673f47abd
ellie <ellie.tms1371@gmail.com>
2021-03-28 21:52:53 +0100

    changed the alt of the image

--- farm-calender/src/Components/Header/Header.js
+++ farm-calender/src/Components/Header/Header.js
@@ -4,17 +4,17 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className=" container">
-      <div className="row">
+    // <div className=" container">
+    //   <div className="row">
       <nav className="navbar headerContainer">
           <img
             src="http://oasisfarmwaterloo.org/wp-content/uploads/2016/06/Website-Logo-Header2-e1465860590865.png"
             className="d-inline-block align-top"
-            alt="oasis farm brand image"
+            alt="oasis farm brand"
           />
       </nav>
-      </div>
-      </div>
+      // </div>
+      // </div>
   );
 };
 
