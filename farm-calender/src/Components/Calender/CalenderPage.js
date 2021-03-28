dac22eb53c2de414b64794651fec5c156a0f0d50
ellie <ellie.tms1371@gmail.com>
2021-03-28 13:22:23 +0100

    added components for calenderpage

---
+++ farm-calender/src/Components/Calender/CalenderPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const CalenderPage = () => {
+    return(
+    <div> 
+    <Header/>
+    <p>Helloo</p>
+    {/* <Footer/> */}
+    </div>   
+    )
+}
+
+
+
+export default CalenderPage;
