ed478d023752b8a4d3f552a8fc7919e6ba6662a0
ellie <ellie.tms1371@gmail.com>
2021-04-11 00:04:13 +0100

    added backend file

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+// view engine setup
+app.engine('handlbars',exphbs);
+app.set('view engine', 'handlebars');
+
+
+
+app.get("/info",(req,res) => {
+    res.send("I am working :P ");
+})
+
+app.post("/user-info",(req,res) => {
+    console.log("ELLIIEEEE");
+})
+
+
+
+app.listen(3001, () => console.log("It is starting ..."));
