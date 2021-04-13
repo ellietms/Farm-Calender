34d4d46cb1a0ebb60a09f2e5f64f426855395cab
ellie <ellie.tms1371@gmail.com>
2021-04-13 23:56:46 +0100

    added mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const mongodb = require("mongodb");
 const nodemailer = require("nodemailer");
 
 const app = express();
